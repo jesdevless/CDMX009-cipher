f8ae6331f3f242750eb9e280e08bd501307dac91
jesdevless <jesica.trejof@gmail.com>
2020-02-15 12:03:53 -0600

    maquetado html y css finalizado y cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,10 +1,36 @@
 const cipher = {
-  encode,
-  decode
+  encode:
+  decode:
 };
 
 export default cipher;
 
-function encode (){
-  
+//codificar el texto-string//
+let solved = "";
+function encode (str, offset){
+  for (let i=0; i<str.lenght; i++){             //bucle para incrementar la longitud del texto y declaración de la variable//
+    let asciiNum = str[i].charCodeAt();         //variable letra código asciiNum//
+    if (asciiNum >=65 && >=90) {                //fórmula cifrado//
+      solved += string.fromCharCode(asciiNum);
+    }
+    else {                                     //suma para incremento código asciiNum//
+      asciiNum = (asciiNum -65 + parseInt(offset))%26+65;
+      solved +=string.fromCharCode(asciiNum)
+    }
+  }
+  return (solved);                              //retorna el objeto resuelto//
 }
+
+
+//decodificar el texto-string//
+let solved = "";
+  function decode (str, offset){
+    for (let i=0; i < str.length; i++) {
+      let asciiNum = str.[i].charCodeAt;
+      if (asciiNum >=65 && >=90) {
+        asciiNum = (asciiNum +65 - parseInt(offset))%26+65;            //fórmula descifrado//
+        solved += string.fromCharCode(asciiNum);
+      }
+    }
+  return(solved);
+  }
