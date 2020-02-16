d5c64529ce43f16681ae1ab92cc5ab2081378dc4
jesdevless <jesica.trejof@gmail.com>
2020-02-16 15:46:31 -0600

    probando funcionalidad js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import cipher from './cipher.js';
 
+//determinar valor (offset) en las rutas asignadas
+function obtenerRuta() {
+
+
+}
+
+
+ruta1[0]. addEventListener ('click', obtenerRuta)
+ruta2[1]. addEventListener ('click', obtenerRuta)
+ruta3[2]. addEventListener ('click', obtenerRuta)
+
+
+//dar click (evento onclick) y obtener el punto de encuentro descifrado
+  document.getElementById('id').innerHTML =
+
+
+//ingresar dirección en input para cifrado
+
+
+//obtención del código cifrado
+
 console.log(cipher);
